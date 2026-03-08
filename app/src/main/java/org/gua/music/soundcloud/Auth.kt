79f4c176dded7ab3bdccf138f9f02e01b15e552f
oxxx1mif <pluginexteragram@gmail.com>
2026-03-08 17:42:15 +1000

    update

---
+++ app/src/main/java/org/gua/music/soundcloud/Auth.kt
@@ -0,0 +1,13 @@
+/*
+ This is the source code for GuAMusic for Android.
+ License: GNU General Public License v.3.
+
+ Copyright 2026 Gleb Obitotsky
+*/
+
+package org.gua.music.soundcloud
+
+import ru.gua.soundcloud.auth.AuthSoundCloud
+
+class Auth {
+}
---
+++ soundcloud/src/main/java/ru/gua/soundcloud/auth/AuthSoundCloud.kt
@@ -0,0 +1,11 @@
+/*
+ This is the source code for GuA SoundCloud library for Android.
+ License: GNU General Public License v.3.
+
+ Copyright 2026 Gleb Obitotsky
+*/
+
+package ru.gua.soundcloud.auth
+
+class AuthSoundCloud {
+}
